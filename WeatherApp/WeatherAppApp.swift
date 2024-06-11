4c75a71f63b48fa507c28a3b7b2d51f432ef726a
Katherine <katherine.e.chambers7@gmail.com>
2024-06-11 20:12:00 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
