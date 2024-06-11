47fb1bda0b7a7f4029e46aba7b8f1bcdd9c48fd5
Katherine <katherine.e.chambers7@gmail.com>
2024-06-11 21:17:11 +0200

    Initial App Set up
    Organise folders and Create Color Extensions

---
+++ WeatherApp/Views/Home/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  WeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            Text("Weather App")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ WeatherApp/Views/Home/HomeViewModal.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModal.swift
+//  WeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/11.
+//
+
+import SwiftUI
+
+struct HomeViewModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewModal()
+}
