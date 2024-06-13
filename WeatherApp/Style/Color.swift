af34b4d4fb7d8ebb5c58d62eeb92afb8cc23f1ea
Katherine <katherine.e.chambers7@gmail.com>
2024-06-13 18:29:42 +0200

    Adds the colours and assets

---
+++ WeatherApp/Style/Color.swift
@@ -0,0 +1,41 @@
+//
+//  Color.swift
+//  WeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/13.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: - Color
+public extension Color {
+    /**
+     #47AB2F
+     */
+    static let sunny = Color(hex: 0x47AB2F, alpha: 1)
+    /**
+     #54717A
+     */
+    static let cloudy = Color(hex: 0x54717A, alpha: 1)
+    /**
+     #57575D
+     */
+    static let rainy = Color(hex: 0x57575D, alpha: 1)
+}
+
+//MARK: - UIColor
+public extension UIColor {
+    /**
+     #47AB2F
+     */
+    static let sunny = UIColor(hex: 0x47AB2F, alpha: 1)
+    /**
+     #54717A
+     */
+    static let cloudy = UIColor(hex: 0x54717A, alpha: 1)
+    /**
+     #47AB2F
+     */
+    static let rainy = UIColor(hex: 0x57575D, alpha: 1)
+}
